627a4de01439fb9461c77d2271ccf6cec4a1c291
Henry Aspegren <henryaspegren@gmail.com>
2018-04-20 16:32:42 -0400

    added api spec

--- src/api/BVerifyProtocolClientAPI.java
+++ src/api/BVerifyProtocolClientAPI.java
@@ -2,12 +2,48 @@ package api;
 
 import java.rmi.Remote;
 
+/**
+ * The API exposed by b_verify clients to the server
+ * 
+ * @author Henry Aspegren
+ *
+ */
 public interface BVerifyProtocolClientAPI extends Remote {
 	
+	/**
+	 * Invoked remotely by the server to request that a client 
+	 * approve an issued receipt. If the client approves, 
+	 * she releases a signature.
+	 * @param approveIssueMessage a serialized proof showing 
+	 * the issued receipt (see IssueReceiptRequest in
+	 * bverifyprotocolapi.proto)
+	 * @return a serialized witness and signature (see Signature in
+	 * bverifyprotocolapi.proto)
+	 */
 	public byte[] approveReceiptIssue(byte[] approveIssueMessage);
 	
+	/**
+	 * Invoked remotely by the server to request that a client 
+	 * approve a redeemed receipt. If the client approves, 
+	 * she releases a signature
+	 * @param approveRedeemMessage a serialized proof showing 
+	 * the redeemed receipt (see RedeemReceiptRequest in
+	 * bverifyprotocolapi.proto)
+	 * @return a serialized witness and signature (see Signature in
+	 * bverifyprotocolapi.proto)
+	 */
 	public byte[] approveReceiptRedeem(byte[] approveRedeemMessage);
 	
+	/**
+	 * Invoked remotely by the server to request that a client 
+	 * approve a receipt transfer. If the client approves,
+	 * she releases a signature
+	 * @param approveTransferMessage a serialized proof showing 
+	 * the transferred receipt (see TransferReceiptRequest in 
+	 * bverfiyprotocolapi.proto)
+	 * @return a serialized witness and signature (see Signature in 
+	 * bverifyprotocolapi.proto)
+	 */
 	public byte[] approveReceiptTransfer(byte[] approveTransferMessage);
 	
 }
